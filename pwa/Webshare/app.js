862a909fd18414267511f3337af2f4f97bbe7d04
Lisandra-blip <79881409+Lisandra-blip@users.noreply.github.com>
2022-09-11 01:10:18 -0300

    Add files via upload

--- pwa/Webshare/app.js
+++ pwa/Webshare/app.js
@@ -1,76 +1,113 @@
-
+// form
 class WebShare extends HTMLElement {
   constructor() {
     // Sempre chame super primeiro no construtor
     super();
-
     // escreva a funcionalidade do elemento aqui
     this.attachShadow({mode: 'open'})
 
     const wrapper = document.createElement('button');
     wrapper.textContent = 'Enviar';
 
+    const title = document.createElement('input');
+    title.textContent = 'title';
+    title.setAttribute('class','title');
+
     this.shadowRoot.append(wrapper);
   }
+
 }
+
 customElements.define('web-share', WebShare);
+// form
+
+// historico
+class Historico extends HTMLElement {
+	constructor() {
+		super();
+		const shadow = this.attachShadow({mode: 'open'});
+
+    var list = localStorage.getItem('storage') ? JSON.parse(localStorage.getItem('storage')) : [];
+    this.listarItens(shadow, list)
+	}
+
+  listarItens(shadow, list) {
+		list.forEach(listItem => {
+			let content = document.getElementById('itens').content;
+			content.getElementById('title').innerHTML = listItem.title ? listItem.title : '';
+			content.getElementById('url').innerHTML = listItem.url ? listItem.url : '';
+      content.getElementById('files').innerHTML = listItem.files ? listItem.files : '';
+			let container = document.createElement("div");
+			container.appendChild(content.cloneNode(true))
+			shadow.appendChild(container);
+		})
+	}
+}
+
+customElements.define('app-historico', Historico);
+//historico
 
 
 
-localStorage.setItem("urlTemplate", title);
-document.getElementById("result").innerText = localStorage.getItem("urlTemplate");
-
-
-  const shareButton = document.querySelector('#share');
-  const fileField = document.querySelector('#file');
-
+//share
+const shareButton = document.querySelector('#share');
+//const fileField = document.querySelector('#file');
 
 shareButton.addEventListener('click', async () => {
 
+// debugger
   const title = document.querySelector('#title').value;
   const text = document.querySelector('#text').value;
   const url = document.querySelector('#url').value;
-  const fileName = document.querySelector('#file-name');
-
-localStorage.setItem("urlTemplate", title);
-document.getElementById("result").innerText = localStorage.getItem("urlTemplate");
-
+  const fileField = document.querySelector('#file');
   const files = fileField ? fileField.files : [];
+  
+  const data = {title, text, url, files};
 
-  const data = {title, text, url};
+//debugger
 
   if(files.length) {
     data.files = files;
   }
 
   try {
+
+    // localStorage
+    let list = localStorage.getItem('storage') ? JSON.parse(localStorage.getItem('storage')) : [];
+    list.push(data);
+    localStorage.setItem('storage', JSON.stringify(list));
+    // localStorage
+
     await navigator.share(data);
+    
   }
   catch(e) {
     console.log('share error', e);
   }
 });
 
-if(fileField) {
-  fileField.addEventListener('change', e => {
-    const {files} = e.target;
-    const {name} = files[0];
+// debugger
+// if(fileField) {
+//   fileField.addEventListener('change', e => {
+//     const {files} = e.target;
+//     const {name} = files[0];
 
-    if(name) {
-      fileName.innerText = name;
-    }
-  });
-} 
+//     if(name) {
+//       fileName.innerText = name;
+//     }
+//   });
+// } 
+//share
 
-
-
+// serviceWorker
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("./serviceWorker.js")
       .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
+      .catch(err => console.log("service worker not registered", err));
+  });
 }
-    
+
+// serviceWorker
